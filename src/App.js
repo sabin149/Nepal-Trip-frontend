dea1708baa4eef08d603a9306808b7909f477d7c
Anish Shrestha <xresthaanish@gmail.com>
2022-05-23 22:14:50 +0545

    Home page added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Register from "./pages/Register"
 import Login from "./pages/Login"
 
 import Hotel from "./pages/hotel/[id]";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div >
       <Router>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
+          
 
 
           <Route path="/login" element={<Login />} />
