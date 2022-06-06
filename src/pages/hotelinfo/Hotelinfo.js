9835c77cd3f34e0b2be516f4433c759036e5073b
DESKTOP-T38SG50\Xrest <xresthaanish@gmail.com>
2022-06-06 23:57:55 +0545

    Star Rating Added

--- src/pages/hotelinfo/Hotelinfo.js
+++ src/pages/hotelinfo/Hotelinfo.js
@@ -368,7 +368,31 @@ const Hotelinfo = () => {
         <div className="segment">
           <h3 className="bold">
             <span>Map</span>
+            <hr/>
           </h3>
+        </div>
+        <div className="segment">
+          <h3 className="bold">
+            <span>Review & Rating </span>
+            <hr/>
+          </h3>
+          <div>
+                <div class="content">
+                  <p>Rating (select a star amount):</p>
+                </div>
+                <div class="wrapper">
+                  <input name="ratingRadio" type="radio" id="st1" value="1" />
+                  <label for="st1"></label>
+                  <input name="ratingRadio" type="radio" id="st2" value="2" />
+                  <label for="st2"></label>
+                  <input name="ratingRadio" type="radio" id="st3" value="3" />
+                  <label for="st3"></label>
+                  <input name="ratingRadio" type="radio" id="st4" value="4" />
+                  <label for="st4"></label>
+                  <input name="ratingRadio" type="radio" id="st5" value="5" />
+                  <label for="st5"></label>
+                </div>
+          </div>
         </div>
         </div>
       </div>
