a58bc5540d54a4a1314fc13d0247c43bc97afcba
DESKTOP-P6L93F6\97798 <suyeshshrestha3@gmail.com>
2022-06-18 15:48:16 +0545

    Update Edit Booking Details

--- src/components/booking/EditBookingDetails.js
+++ src/components/booking/EditBookingDetails.js
@@ -2,23 +2,72 @@ import React from 'react'
 
 const EditBookingDetails = () => {
   return (
-    <div>EditBookingDetails</div>
-  )
-}
-
-<div className='container'>
+    <div>
+            <div className='container'>
     <div className='row'>
       <div className='col-3'>
-        <h3>col-1</h3>
+        
       </div>
-      <div className='col-6'>
-          <h3>col-2</h3>
+      <div className='col-6 mt-5'>
+             <div className="hotel_label">
+                    <label className="form-label">Hotel Name</label>
+                    <input
+                      type="text"
+                      className="form-control hotel_input"
+                      id="exampleInputEmail1"
+                      name="hotel_name"
+                    />
+             </div>
+             
+             <div className="hotel_label">
+                    <label className="form-label">Hotel Name</label>
+                    <input
+                      type="text"
+                      className="form-control hotel_input"
+                      id="exampleInputEmail1"
+                      name="hotel_name"
+                    />
+             </div>
+
+             <div className="hotel_label">
+                    <label className="form-label">Hotel Name</label>
+                    <input
+                      type="text"
+                      className="form-control hotel_input"
+                      id="exampleInputEmail1"
+                      name="hotel_name"
+                    />
+             </div>
+
+             <div className="hotel_label">
+                    <label className="form-label">Hotel Name</label>
+                    <input
+                      type="text"
+                      className="form-control hotel_input"
+                      id="exampleInputEmail1"
+                      name="hotel_name"
+                      
+                    />
+             </div>
+
+             <button
+                    type="submit"
+                    className="btn btn-warning w-25 mb-4 mt-4 "
+                  >
+                    Save
+                  </button>
       </div>
       <div className='col-3'>
-          <h3>col-3</h3>
+          
       </div>
     </div>
 
 </div>
+    </div>
+    
+  )
+}
+
+
 
 export default EditBookingDetails
