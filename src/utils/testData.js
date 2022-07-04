d31f358af9c77e25f1a68b35f3efe224d0e93a68
Ravi Mandal <ravimandal47@hotmail.com>
2022-07-04 21:28:14 +0545

    fixed test data

--- src/utils/testData.js
+++ src/utils/testData.js
@@ -2875,7 +2875,18 @@
     {
       "id": "ZPL-2.0",
       "identifiers": [
-        
+        {
+          "identifier": "Zope-2.0",
+          "scheme": "DEP5"
+        },
+        {
+          "identifier": "ZPL-2.0",
+          "scheme": "SPDX"
+        },
+        {
+          "identifier": "License :: OSI Approved :: Zope Public License",
+          "scheme": "Trove"
+        }
       ],
       "links": [
         {
